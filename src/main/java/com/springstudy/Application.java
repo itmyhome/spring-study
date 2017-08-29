c07efff57334e99d6b996fadeb1d4617cdbc540e
xie t <xietao@shicaidai.com>
2017-08-29 10:58:12 +0800

    商品列表 订单列表

--- src/main/java/com/springstudy/Application.java
+++ src/main/java/com/springstudy/Application.java
@@ -1,13 +1,9 @@
 package com.springstudy;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableAutoConfiguration
 @SpringBootApplication
-@EnableJpaRepositories("com.springstudy.repository")
 public class Application {
 
     public static void main(String[] args) {
